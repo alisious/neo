5a452bc89463d59879cf5e8a668ea07d44823721
alisious <alisious@wp.pl>
2014-11-25 09:48:26 +0100

    Dodanie do UnitOfWork obsługi słowników.

--- Kseo2/Service/UnitOfWork.cs
+++ Kseo2/Service/UnitOfWork.cs
@@ -1,4 +1,6 @@
-﻿using Kseo2.DataAccess;
+﻿using System.Runtime.InteropServices;
+using Kseo2.DataAccess;
+using Kseo2.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,22 +12,40 @@ namespace Kseo2.Service
     public class UnitOfWork :IUnitOfWork
     {
         private readonly KseoContext _context;
+        private List<Country> _countries;
+        private readonly DictionaryService<Country> _countryService;  
+
 
         public UnitOfWork()
         {
             _context = new KseoContext();
+            _countryService = new DictionaryService<Country>(_context);
         }
-
-
-
+        
         public KseoContext Context()
         {
             return _context;
         }
+        
 
         public void SaveChanges()
         {
             _context.SaveChanges();
+        }
+
+        public List<Country> Countries
+        {
+            get
+            {
+                if (_context == null)
+                    _countries = _countryService.GetAllByDisplayOrder(null);
+                return _countries;
+            }
+        }
+
+        public void LoadDictionary(Type type)
+        {
+            if (type == typeof (Country)) _countries = _countryService.GetAllByDisplayOrder(null);
         }
     }
 }
