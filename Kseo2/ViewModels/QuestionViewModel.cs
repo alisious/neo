fc1d174a4ab46d440a4d5ade5cc7a269c85ada3b
alisious <alisious@wp.pl>
2014-12-08 23:25:59 +0100

    Dodanie QuestionView

---
+++ Kseo2/ViewModels/QuestionViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Kseo2.Model;
+
+namespace Kseo2.ViewModels
+{
+    public class QuestionViewModel :Screen
+    {
+        private Question _question;
+        
+        
+        public QuestionViewModel()
+        {
+            _question = new Question();
+        }
+
+        public QuestionViewModel(Question question)
+        {
+            _question = question;
+        }
+
+        public Question Question
+        {
+            get { return _question; }
+            set
+            {
+                _question = value;
+                NotifyOfPropertyChange(()=>Question);
+            }
+
+        }
+
+    }
+}
