48186dd9233d8ec9603135f84780e83dd55579f7
alisious <alisious@wp.pl>
2015-02-22 23:19:06 +0100

    Dodanie PersonBaseViewModel

---
+++ Kseo2/ViewModels/Common/BaseViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Caliburn.Micro.Validation;
+using Kseo2.ViewModels.Events;
+using System.Runtime.CompilerServices;
+
+namespace Kseo2.ViewModels.Common
+{
+    public class BaseViewModel<T> :ValidatingScreen<T>, IHandle<IsDirtyEvent>,IHandle<CanSaveEvent> where T :ValidatingScreen<T>
+    {
+        private bool _isDirty;
+        
+        public BaseViewModel(IEventAggregator events)
+        {
+            Events = events;
+        }
+
+        public IEventAggregator Events { get; private set; }
+
+        public bool IsDirty
+        {
+            get { return _isDirty; }
+            set
+            {
+                _isDirty = value;
+                NotifyOfPropertyChange(() => IsDirty);
+                if (_isDirty) Events.PublishOnUIThread(new IsDirtyEvent(true));
+            }
+        }
+
+        public virtual bool CanSave
+        {
+            get { return true; }
+        }
+
+        protected void OnPropertyChanged(object value, [CallerMemberName] string propertyName = "")
+        {
+            NotifyOfPropertyChange(propertyName);
+            NotifyOfPropertyChange(() => CanSave);
+            Events.PublishOnUIThread(new CanSaveEvent(CanSave));
+            IsDirty = true;
+        }
+        
+        public void Handle(IsDirtyEvent message)
+        {
+            IsDirty = IsDirty || message.IsDirty;
+        }
+
+        public void Handle(CanSaveEvent message)
+        {
+           NotifyOfPropertyChange(()=>CanSave);
+        }
+    }
+}
--- Kseo2/ViewModels/PersonBaseViewModel.cs
+++ Kseo2/ViewModels/PersonBaseViewModel.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 using Caliburn.Micro.Validation;
+using Kseo2.ViewModels.Common;
 
 namespace Kseo2.ViewModels
 {
-    public class PersonBaseViewModel :ValidatingScreen<PersonBaseViewModel>
+    public class PersonBaseViewModel :BaseViewModel<PersonBaseViewModel>
     {
+        public PersonBaseViewModel(IEventAggregator events) : base(events)
+        {
+
+        }
 
     }
 }
